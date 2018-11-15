b546883521a3a11cccba4dc749fc236c156354e2
Philip Lin <wei-hsin.lin@clarivate.com>
2018-11-15 15:36:43 -0800

    Simplified constructor

--- src/Person.java
+++ src/Person.java
@@ -7,23 +7,9 @@ public class Person {
         this.name = name;
         this.gender = gender.toUpperCase();
         this.fixed_group = fixed_group;
-
-        if (talk.toUpperCase().equals("Y")) {
-            this.talk = true;
-        } else {
-            this.talk = false;
-        }
-
-        if (christian.toUpperCase().equals("Y")) {
-            this.christian = true;
-        } else {
-            this.christian = false;
-        }
-        if (counselor.toUpperCase().equals("Y")) {
-            this.counselor = true;
-        } else {
-            this.counselor = false;
-        }
+        this.talk = talk.toUpperCase().equals("Y");
+        this.christian = christian.toUpperCase().equals("Y");
+        this.counselor = counselor.toUpperCase().equals("Y");
     }
 
     public Person() {
